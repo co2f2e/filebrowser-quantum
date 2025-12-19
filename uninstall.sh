52c8e4085d465a1dcd21b74c80a67cd807dbfa7f
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-12-20 03:41:16 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,37 +5,45 @@ APP_NAME="filebrowser"
 BIN_PATH="/usr/local/bin/${APP_NAME}"
 CONFIG_DIR="/etc/filebrowser"
 SERVICE_FILE="/etc/systemd/system/${APP_NAME}.service"
-FILE="/filebrowser_quantum"
+STORAGE_NAME="/filebrowser_quantum_storage"
 
 echo "Stopping FileBrowser service..."
 if systemctl is-active --quiet ${APP_NAME}; then
-    sudo systemctl stop ${APP_NAME}
+    systemctl stop ${APP_NAME}
 fi
 
 echo "Disabling FileBrowser service..."
 if systemctl is-enabled --quiet ${APP_NAME}; then
-    sudo systemctl disable ${APP_NAME}
+    systemctl disable ${APP_NAME}
 fi
 
 echo "Removing systemd service file..."
 if [ -f "${SERVICE_FILE}" ]; then
-    sudo rm -f "${SERVICE_FILE}"
-    sudo systemctl daemon-reload
+    rm -f "${SERVICE_FILE}"
+    systemctl daemon-reload
 fi
 
 echo "Removing binary..."
 if [ -f "${BIN_PATH}" ]; then
-    sudo rm -f "${BIN_PATH}"
+    rm -f "${BIN_PATH}"
 fi
 
 echo "Removing configuration directory..."
 if [ -d "${CONFIG_DIR}" ]; then
-    sudo rm -rf "${CONFIG_DIR}"
+    rm -rf "${CONFIG_DIR}"
 fi
 
-echo "Removing directory..."
-if [ -d "${FILE}" ]; then
-    sudo rm -rf "${FILE}"
+if [ -d "${STORAGE_NAME}" ]; then
+    read -p "Directory ${STORAGE_NAME} already exists. Do you want to keep existing files? [y/N]: " keep
+    case "$keep" in
+        [yY]|[yY][eE][sS])
+            echo "Keeping existing files."
+            ;;
+        *)
+            echo "Deleting existing directory..."
+            rm -rf "${STORAGE_NAME}"
+            ;;
+    esac
 fi
 
 echo "FileBrowser has been completely uninstalled."
